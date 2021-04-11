96dc36d1848ddf8debaf4e712e899f15421b1a95
AntonioTimothy <greenlightkibe@gmail.com>
2021-04-11 07:19:38 +0000

    Deploying to gh-pages from  @ 7d5cf2c4648f4a4dabec19cf9d6cac7b193feec8 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.020c124d1f56a37c94ebcfba94607ba7.js"
+  "/precache-manifest.2258bcb04542869be49f3b7a7b9dc9f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
